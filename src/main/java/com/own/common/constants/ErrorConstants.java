0811e76c3e0bc9b189f4e091d4986820a4487b0e
Ankul Rastogi <ankul.rastogi@gmail.com>
2012-05-21 19:11:50 +0530

    Error Constants

---
+++ src/main/java/com/own/common/constants/ErrorConstants.java
@@ -0,0 +1,21 @@
+package com.own.common.constants;
+
+import java.util.Map;
+
+public class ErrorConstants {
+
+	public static final Integer USER_ALREADY_EXISTS = 11;
+	//General Error constant only for logging purpose
+	public static final Integer LOG = -1;
+	public static final Integer DUPLICATE_MERCHANT = 15;
+	public static final Integer MERCHANT_NOT_FOUND_EMAIL = 12;
+	public static final Integer MERCHANT_NOT_FOUND_EMAIL_USERNAME = 13;
+	public static final Integer PASSWORD = 14;
+	public static final Integer REGISTRATION_NOT_ACTIVE = 15;
+	public static final Integer ACTIVATION_EXPIRED = 16;
+	public static final Integer ACTIVATION_USED = 17;
+	public static final Integer INVALID_ACTIVATION_LINK = 18;
+	public static final Integer ERROR = -1;
+	public static final Integer DATABASE_ERROR = 20;
+
+}
