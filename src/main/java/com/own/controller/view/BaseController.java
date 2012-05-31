2ec8438194bec5ee909969ae4e9cda7d3b6e16cf
Ankul Rastogi <ankul.rastogi@gmail.com>
2012-05-31 18:32:37 +0530

    base controller query string.. hibernate fail fast thing

--- src/main/java/com/own/controller/view/BaseController.java
+++ src/main/java/com/own/controller/view/BaseController.java
@@ -3,6 +3,8 @@ package com.own.controller.view;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.ui.Model;
 
@@ -144,5 +146,54 @@ public class BaseController {
 	private void addUIMessage(final Model model, final String key) {
 		model.addAttribute(AppConstant.HASMESSAGES_KEY, true);
 		model.addAttribute(AppConstant.MESSAGE_KEY, key);
+	}
+	
+	private String convertToQueryString(final Map<String, String[]> paramMap) {
+
+		StringBuffer buffer = new StringBuffer();
+		for (String key : paramMap.keySet()) {
+			for (String element : paramMap.get(key)) {
+				buffer.append(key).append("=").append(element).append("&");
+			}
+		}
+		buffer.deleteCharAt(buffer.length() - 1);
+		return buffer.toString();
+	}
+	
+	protected String getInternalRedirectView(final HttpServletRequest request,
+			final String controllerPath, final Map<String, String> param) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String[]> parameterMap = new HashMap<String, String[]>(
+				request.getParameterMap());
+
+		// see if any element is to be updated in parameter Map. If its null
+		// then simply ignore it
+		if (null != param) {
+			for (String key : param.keySet()) {
+				parameterMap.put(key, new String[] { param.get(key) });
+			}
+
+		}
+
+		String queryString = convertToQueryString(parameterMap);
+
+		return getInternalRedirectView(controllerPath, queryString);
+	}
+	protected String getInternalRedirectView(final String name,
+			final String queryString) {
+		if (null == queryString) {
+			return getInternalRedirectView(name);
+		}
+		return new StringBuilder().append(AppConstant.REDIRECT).append(name)
+				.append(AppConstant.EXTENSION).append("?").append(queryString)
+				.toString();
+
+	}
+	
+	protected String getInternalRedirectView(final String name) {
+		return new StringBuilder().append(AppConstant.REDIRECT).append(name)
+				.append(AppConstant.EXTENSION).toString();
+
 	}
 }
